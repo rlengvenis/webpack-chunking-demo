7b52142e579f158fcd120e9f41877af7dd0d41d1
Rokas Lengvenis <rokas.lengvenis@gmail.com>
2019-09-18 14:48:36 +0100

    Add source-maps

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-
+  devtool: "source-map",
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),

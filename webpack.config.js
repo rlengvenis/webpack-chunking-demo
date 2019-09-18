7d45c6b3b46d7e0d7d945a31b19df9616c2da9af
Rokas Lengvenis <rokas.lengvenis@gmail.com>
2019-09-18 15:07:06 +0100

    Add static chunking example

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,25 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   devtool: "source-map",
+
+  // SplitChunksPlugin
+  optimization: {
+    splitChunks: {
+      name: false,
+      minSize: 0,
+      cacheGroups: Object.assign({
+        packages: {
+          name: "packages",
+          test: module => module.context && module.context.includes("smallest"),
+          chunks: "initial"
+        },
+        default: {
+          name: "main",
+        }
+      })
+    }
+  },
+
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
